738275386b21598e0a6a4ad49d41e199c0bb83d2
yinm <yinm.cminor9@gmail.com>
2017-09-16 08:43:23 +0900

    Change signup path

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,7 +4,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test 'invalid signup information' do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, params: {
+      post '/signup', params: {
           user: {
               name: '',
               email: 'user@invalid',
