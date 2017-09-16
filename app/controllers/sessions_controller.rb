f54c54a7408a3d0400b3a3d6d5753da3ae405ff0
yinm <yinm.cminor9@gmail.com>
2017-09-16 10:20:25 +0900

    Add authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+
+    if user && user.authenticate(params[:session][:password])
+      # redirect to user information page after login
+    else
+      # create error messages
+      render 'new'
+    end
   end
 
   def destroy
