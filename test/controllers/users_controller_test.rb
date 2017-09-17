c6d2bd3a07e82b2c411d4121240f56a9d8914a3c
yinm <yinm.cminor9@gmail.com>
2017-09-17 13:34:00 +0900

    Add test of permission denied for updating admin flag

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -15,4 +15,17 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     get users_path
     assert_redirected_to login_url
   end
+
+  test 'should not allow the admin attribute to be edited via the web' do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+        user: {
+            password: @other_user.password,
+            password_confirmation: @other_user.password,
+            admin: true,
+        }
+    }
+    assert_not @other_user.reload.admin?
+  end
 end
